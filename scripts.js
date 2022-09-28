ee3c57d16d3a39f67276419a347a66aacb26ebc7
Brian <briangordon.cahill@students.dominican.edu>
2022-09-28 16:51:33 -0700

    cart fully functional

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const itemList = document.getElementById('item-list');
 const cartQty = document.getElementById('cart-qty');
 const cartTotal = document.getElementById('cart-total');
 
+
 const cart = []
 
 
@@ -44,6 +45,35 @@ for (let i = 0; i < data.length; i += 1) {
 const all_items_button = Array.from(document.querySelectorAll("button"))
 
 
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItem()
+}))
+
+itemList.onchange = function(e) {
+    if (e.traget && e.target.classList.contains('update')) {
+        const name = e.target.dataset.name;
+        const qty = parseInt(e.target.value);
+        updateCart(name, qty);
+    }
+}
+
+itemList.onclick = function(e) {
+    if (e.target && e.target.classList.contains('remove')) {
+        const name = e.target.dataset.name;
+        removeItem(name)
+    }
+    else if (e.target && e.target.classList.contains('add-one')) {
+        const name = e.target.dataset.name;
+        addItem(name)
+    }
+    else if (e.target && e.target.classList.contains('remove-one')) {
+        const name = e.target.dataset.name;
+        removeItem(name, 1)
+    }
+}
+
+
 function addItem(name, price) {
     for (let i = 0; i < cart.length; i++) {
         if (cart[i].name === name) {
@@ -55,6 +85,7 @@ function addItem(name, price) {
     const item = {name, price, qty: 1}
     cart.push(item);
 }
+
 function showItem() {
     let itemStr = '';
     const qty = getQty();
@@ -65,7 +96,13 @@ function showItem() {
         // console.log(`-${cart[i].name} ${cart[i].price} x ${cart[i].qty}`);
         const { name, price, qty } = cart[i]
 
-        itemStr += `<li>${name} $${price} x ${qty} = ${qty * price}</li>`;
+        itemStr += `<li>
+            ${name} $${price} x ${qty} = ${qty * price}
+            <button class="remove" data-name="${name}">Remove</button>
+            <button class="add-one" data-name="${name}"> + </button>
+            <button class="remove-one" data-name="${name}"> - </button>
+            <input class="update" type="number" data-name="${name}">
+        </li>`;
     }
     itemList.innerHTML = itemStr;
     // console.log(`Total in cart: $${getTotal()}`);
@@ -95,18 +132,28 @@ function removeItem(name, qty = 0) {
             if (cart[i].qty < 1 || qty === 0){
                 cart.splice(i, 1)
             }
+            showItem()
             return
         }
     }
 }
 
-all_items_button.forEach(elt => elt.addEventListener('click', () => {
-    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
-    showItem()
-}))
+function updateCart(name, qty) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name)
+                return
+            }
+            cart[i].qty = qty;
+            showItem();
+            return
+        }
+    }
+}
 
 
 
 showItem()
-addItem('Apple', 0.99)
-addItem('Orange', 1.29)
+// addItem('Apple', 0.99)
+// addItem('Orange', 1.29)
